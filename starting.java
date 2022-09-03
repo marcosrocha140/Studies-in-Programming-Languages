898cd2bde929c4a6fe04a17d95ae45d00a073592
Marcos Rocha <rmarcos140@gmail.com>
2022-09-03 14:36:55 -0300

    calculator

--- starting.java
+++ starting.java
@@ -2,25 +2,21 @@ import java.util.Scanner;
 class HelloWorld {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         int x;
-        int y;
 
-        System.out.print("Digite um numero.");
+        System.out.print("Calculadora Online. \n");
+        System.out.print("Qual o numero vai escolher para multiplicar? \n");
+
         x = input.nextInt();
-        System.out.print("Digite outro numero.");
-        y = input.nextInt();
+        int multi;
 
+        for(int i = 1; i<=10; i++){
+            multi = x * i;
+            System.out.println(x +" X "+ i+ " = "+multi);
 
-        while(x != y){
-            System.out.print("Digite um numero.");
-            x = input.nextInt();
-            System.out.print("Digite outro numero.");
-            y = input.nextInt();
-    
         }
 
-        System.out.print("voce saiu do loop: "+ x+y);
-   
 
     }
 }
