7fb3acd3d41601d0e5d16673dc852614387e6dbc
Marcos Rocha <rmarcos140@gmail.com>
2022-09-08 12:34:52 -0300

    Math Operations

--- starting.java
+++ starting.java
@@ -1,21 +1,52 @@
+
 import java.util.Scanner;
 class HelloWorld {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        int multi;
         int x;
+        int a;
+        float divisao;
 
         System.out.print("Calculadora Online. \n");
-        System.out.print("Qual o numero vai escolher para multiplicar? \n");
+        System.out.print("Qual o numero vai escolher? \n");
+         x = input.nextInt();
 
-        x = input.nextInt();
-        int multi;
+        System.out.print("1 = Soma.\n2 = Multiplicar.\n3 = Subtrair.\n4 = Dividir.\n" );
+            a = input.nextInt();
+     
+            switch(a){
+                case 1:
 
-        for(int i = 1; i<=10; i++){
-            multi = x * i;
-            System.out.println(x +" X "+ i+ " = "+multi);
+                for(int i = 1; i<=10; i++){
+                    multi = x + i;
+                    System.out.println(x +" + "+ i+ " = "+multi);
+        
+                }break;
+                case 2:
+                for(int i = 1; i<=10; i++){
+                    multi = x * i;
+                    System.out.println(x +" X "+ i+ " = "+multi);
+                    
+                }break;
+                case 3:
+                for(int i = 1; i<=10; i++){
+                    divisao = x - i;
+                    System.out.println(x +" - "+ i+ " = "+divisao);
+        
+                }break;
+                case 4:
+                for(int i = 1; i<=10; i++){
+                    divisao = x / i;
+                    System.out.println(x +" / "+ i+ " = "+divisao);
+        
+                }break;
+                
+                default:
+                System.out.print("Valor invalido! Informe um numero valido.s");
 
-        }
+            }
 
 
     }
